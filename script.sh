5c909039ae19ff6db7d58ab10bc8bd4408072696
CTWO2000 <tan.wengonn@hotmail.com>
2023-05-01 16:32:42 +0800

    config setup

--- script.sh
+++ script.sh
@@ -2,14 +2,36 @@
 
 set -e
 
-# Tmux Setup 
-sudo apt install tmux 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Install Tmux Plugin Manager
+sudo apt update
+
+# Tmux Setup
+
+# Check if tmux is already installed 
+if [[ ! $(command -v tmux) ]]
+then
+    sudo apt install tmux -y 
+fi
+
+if [[ ! -d "/home/ubuntu/.tmux/plugins/tpm" ]]
+then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Install Tmux Plugin Manager
+fi
+
 mv .tmux.conf ~/ # In Tmux press "ctrl+I" to install the plugins
 
 # NeoVim Setup 
-sudo apt install neovim
-sudo apt install curl 
+# Check if NeoVim is already installed
+if [[ ! $(command -v nvim) ]]
+then
+    sudo apt install neovim -y
+fi
+
+# Check if curl is already installed
+if [[ ! $(command -v curl) ]]
+then 
+    sudo apt install curl -y
+fi
+
 mkdir -p ~/.config/nvim
 mv init.vim ~/.config/nvim/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
